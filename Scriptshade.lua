9b8288b3945a6c316a741b6506c5c2b913fa5778
| R Y A N <ryan.gamer3572@gmail.com>
2026-09-06 16:56:28 +0100

    Update Scriptshade.lua

--- Scriptshade.lua
+++ Scriptshade.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(game:HttpGet("https://raw.githubusercontent.com/Idkwhatt0d0/ScriptShade/refs/heads/main/3f930973-74b7-447f-a8a7-12576163fa27.lua?token=GHSAT0AAAAAAEHLXW7IZ5U35OC5T5F4HYFQ2U5P3DA")))()
+loadstring(game:HttpGet(game:HttpGet("https://raw.githubusercontent.com/Idkwhatt0d0/ScriptShade/refs/heads/main/3f930973-74b7-447f-a8a7-12576163fa27.lua")))()
